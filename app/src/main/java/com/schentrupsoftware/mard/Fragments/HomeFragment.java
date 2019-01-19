051b78594fc7ba660f0d491a8990fc259707a8af
joeySchentrup <j03y@live.com>
2019-01-19 08:56:25 -0500

    Added basic pages
    Signed-off-by: joeySchentrup <j03y@live.com>

---
+++ app/src/main/java/com/schentrupsoftware/mard/Fragments/HomeFragment.java
@@ -0,0 +1,17 @@
+package com.schentrupsoftware.mard.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.schentrupsoftware.mard.R;
+
+public class HomeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home, container, false);
+    }
+}
--- app/src/main/java/com/schentrupsoftware/mard/MainActivity.java
+++ app/src/main/java/com/schentrupsoftware/mard/MainActivity.java
@@ -3,13 +3,24 @@ package com.schentrupsoftware.mard;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.schentrupsoftware.mard.Fragments.HomeFragment;
+import com.schentrupsoftware.mard.Fragments.ListFragment;
+import com.schentrupsoftware.mard.Fragments.ScanFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private static final int HOME_POS = 0;
+    private static final int SCAN_POS = 1;
+    private static final int LIST_POS = 2;
+
+    private NoSwipePager viewPager;
+    private BottomBarAdapter pagerAdapter;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +29,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    viewPager.setCurrentItem(HOME_POS);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    viewPager.setCurrentItem(SCAN_POS);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    viewPager.setCurrentItem(LIST_POS);
                     return true;
             }
             return false;
@@ -35,10 +46,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViewPager();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void setupViewPager() {
+        viewPager = (NoSwipePager) findViewById(R.id.viewpager);
+        viewPager.setPagingEnabled(false);
+        pagerAdapter = new BottomBarAdapter(getSupportFragmentManager());
+
+        pagerAdapter.addFragments(new HomeFragment());
+        pagerAdapter.addFragments(new ScanFragment());
+        pagerAdapter.addFragments(new ListFragment());
+
+        viewPager.setAdapter(pagerAdapter);
     }
 
 }
